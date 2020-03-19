65f0d4beeb3e9204772cc1cd25d51a95a632b734
Emma Romero <emmayrs17@gmail.com>
2020-03-19 09:37:03 -0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:plants/info_plant.dart';
+import 'package:tflite/tflite.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  bool _isLoading;
+  File _image;
+  List _output;
+  String _inf;
+  String _te;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _isLoading = true;
+    loadModel().then((value) {
+      setState(() {
+        _isLoading = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Plants"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: _isLoading ? Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ):SingleChildScrollView( child: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            _image == null ? Container():Image.file(_image, width: 500, height:500),
+            SizedBox(
+              height:16,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            _output == null ? Text("Check your plant"): Text("${_output[0]["label"]}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            _inf == null ? Text(""): Text(_inf.toString()),
+            _te == null ? Text(""): Text(_te.toString()),
           ],
         ),
+      ),scrollDirection: Axis.vertical,
+          reverse:false
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        children: [
+          SpeedDialChild(
+            child:Icon(Icons.image),
+            label: "Gallery",
+            onTap: (){
+              chooseImageGallery();
+            }
+          ),
+          SpeedDialChild(
+              child:Icon(Icons.camera),
+              label: "Camera",
+              onTap: (){
+                chooseImageCamera();
+              }
+          )
+
+        ]
+      ),
     );
   }
+
+
+  chooseImageGallery() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if(image == null) return null;
+    setState(() {
+      _isLoading = true;
+      _image = image;
+    });
+    runModelOnImage(image);
+  }
+
+  chooseImageCamera() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    if(image == null) return null;
+    setState(() {
+      _isLoading = true;
+      _image = image;
+    });
+    runModelOnImage(image);
+  }
+
+
+  runModelOnImage(File image)async{
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      threshold: 0.5
+    );
+    setState(() {
+      _isLoading = false;
+      _output = output;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,10 @@ import 'package:plants/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
